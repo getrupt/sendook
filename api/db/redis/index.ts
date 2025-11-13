499c887d94d0a4eceea7dc4fb79109b14a2da02f
Ahmed Saleh <ahmed@rupt.dev>
2025-11-13 11:54:24 -0800

    Log the host

--- api/db/redis/index.ts
+++ api/db/redis/index.ts
@@ -1,6 +1,7 @@
 import Redis from "ioredis";
 
 export const redis = new Redis({ host: process.env.REDIS_HOST });
+console.log("REDIS_HOST", process.env.REDIS_HOST);
 
 redis.on("connect", () => console.log("Redis connected"));
 redis.on("error", (err) => console.error(err));
