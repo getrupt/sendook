564afd82961d80dba5f48ec2519c553031620074
Marc Whitbread <marc@rupt.dev>
2025-11-13 18:53:12 -0500

    env fixes

--- app/nuxt.config.ts
+++ app/nuxt.config.ts
@@ -1,8 +1,3 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
-const apiUrl =
-  (globalThis as { process?: { env?: Record<string, string | undefined> } }).process?.env?.API_URL ??
-  'https://api.sendook.com';
-
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
@@ -32,7 +27,7 @@ export default defineNuxtConfig({
 
   runtimeConfig: {
     public: {
-      apiUrl
+      apiUrl: process.env.API_URL
     }
   }
 })
