947938692120f389cab16547c2a423e37cba94eb
Marc Whitbread <marc@rupt.dev>
2025-11-13 11:41:10 -0500

    added console

--- api/routes/webhooks/ses.ts
+++ api/routes/webhooks/ses.ts
@@ -4,6 +4,7 @@ import { handleDeliveryNotification } from "../../controllers/SESController";
 const router = Router();
 
 router.post("/", async (req, res) => {
+  console.log("Received SES delivery notification", req.body);
   const payload = JSON.parse(req.body);
   if (payload.Type !== "Notification") {
     return res.status(200).send();
