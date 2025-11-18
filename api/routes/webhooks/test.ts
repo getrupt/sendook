1938a5599bd35c32e2091fd2209694c98ccedd5c
Marc Whitbread <marc@rupt.dev>
2025-11-18 11:22:10 -0500

    pr feedback changes

--- api/routes/webhooks/test.ts
+++ api/routes/webhooks/test.ts
@@ -3,7 +3,6 @@ import { Router } from "express";
 const router = Router();
 
 router.post("/", async (req, res) => {
-  console.log("Received test webhook", req.body);
   return res.status(200).send();
 });
 
