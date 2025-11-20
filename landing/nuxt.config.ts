e8f9b891bbd67969dc07a66fecb561f3aeb37fa2
marcrupt <148103669+marcrupt@users.noreply.github.com>
2025-11-20 14:25:21 -0500

    disable dark mode (#29)

--- landing/nuxt.config.ts
+++ landing/nuxt.config.ts
@@ -30,6 +30,10 @@ export default defineNuxtConfig({
 
   css: ['~/assets/css/main.css'],
 
+  ui: {
+    colorMode: false
+  },
+
   routeRules: {
     '/docs': { redirect: '/docs/getting-started', prerender: false }
   },
